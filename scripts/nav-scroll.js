2bc3ba5237b9648a645b3a6849d81b464bac360e
HbibSfaxi <94782180+HbibSfaxi@users.noreply.github.com>
2021-12-26 10:54:56 +0100

    Add files via upload

---
+++ scripts/nav-scroll.js
@@ -0,0 +1,24 @@
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("changenav").style.padding = "10px";
+    document.getElementById("logo").style.fontSize = "25px";
+  } else {
+    document.getElementById("changenav").style.padding = "70px 10px";
+  }
+}
+
+$(window).scroll(function () {
+  if ($(window).scrollTop() > 10) {
+    $("#changenav").addClass("floatingNav");
+    $("#changenav").addClass("navbar-dark");
+    $("#changenav").removeClass("navbar-light");
+  } else {
+    $("#changenav").removeClass("floatingNav");
+    $("#changenav").removeClass("navbar-dark");
+    $("#changenav").addClass("navbar-light");
+  }
+});
